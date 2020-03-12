2fbbb0422e25a1ea6ed0854b5c8ec348f8f55224
lixiongxiong <PiwOVyjbZx82oL5H6S9Y>
2020-03-12 15:08:00 +0800

    pdf读取为字符串

---
+++ src/main/java/com/lx/application/PdfReader.java
@@ -0,0 +1,10 @@
+package com.lx.application;
+
+public class PdfReader {
+	public static void main(String[] args) {
+		PDFUtil pdfUtil = new PDFUtil();
+		//E:\\tmp\\pdf\\test11.pdf
+		pdfUtil.readPDF("D:\\text.pdf");
+	}
+
+}
